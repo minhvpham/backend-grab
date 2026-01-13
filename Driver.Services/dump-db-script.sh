3c2adc493c76780b650d37b07aff6938e53cd9cb
Duyyy123 <trinhchanduybojack@gmail.com>
2026-01-14 01:27:40 +0700

    build: db dump

--- Driver.Services/dump-db-script.sh
+++ Driver.Services/dump-db-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export PGPASSWORD="1"
+export POSTGRES_PASSWORD="postgres"
+export POSTGRES_DB="driver_services"
+export POSTGRES_USER="postgres"
+export POSTGRES_HOST="localhost"
+export POSTGRES_PORT="5433"
 
-pg_dump -U postgres -h localhost -d postgres >dump/db/db_dump.sql
+pg_dump -U $POSTGRES_USER -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB >dump/db/db_dump.sql
