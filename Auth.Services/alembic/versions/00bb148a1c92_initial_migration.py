fcbbb5412e637bc443eefa4145b00c60dd435f40
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2026-01-19 16:57:32 +0000

    finish

--- Auth.Services/alembic/versions/00bb148a1c92_initial_migration.py
+++ Auth.Services/alembic/versions/00bb148a1c92_initial_migration.py
@@ -1,8 +1,8 @@
-"""create users table
+"""initial migration
 
-Revision ID: 3ea547b29268
+Revision ID: 00bb148a1c92
 Revises: 
-Create Date: 2026-01-11 11:22:53.075185
+Create Date: 2026-01-19 16:54:43.197352
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '3ea547b29268'
+revision: str = '00bb148a1c92'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -25,6 +25,11 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('hashed_password', sa.String(), nullable=False),
+    sa.Column('role', sa.Enum('user', 'seller', 'shipper', 'admin', name='roleenum'), nullable=True),
+    sa.Column('status', sa.Enum('active', 'reject', 'inactive', 'pending', 'banned', name='user_status_enum'), nullable=False),
+    sa.Column('is_deleted', sa.Boolean(), nullable=True),
+    sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
+    sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
